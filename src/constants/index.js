064bbb2a2b0cf97dc0516b14709ad7cad4d8b3f0
Vicky H <63320567+vicky0h@users.noreply.github.com>
2023-06-29 10:58:21 +0800

    Update index.js project

--- src/constants/index.js
+++ src/constants/index.js
@@ -142,6 +142,123 @@ import {
     },
   ];
   
+  // const projects = [
+  //   {
+  //     name: "Dental Clinic",
+  //     description:
+  //       "Web-based platform that allows users to search, book, and manage dental appointments from various locations, providing a convenient and efficient solution for dental clinic booking needs.",
+  //     tags: [
+  //       {
+  //         name: "react",
+  //         color: "blue-text-gradient",
+  //       },
+  //       {
+  //         name: "mongodb",
+  //         color: "green-text-gradient",
+  //       },
+  //       {
+  //         name: "tailwind",
+  //         color: "pink-text-gradient",
+  //       },
+  //     ],
+  //     image: dentalclinic,
+  //     source_code_link: "https://github.com/vicky0h/DentalClinic.git",
+  //   },
+  //   {
+  //     name: "Vacationbnb",
+  //     description:
+  //       "Web application that enables users to search for and post short-term rentals for vacations, and locate available rentals based on location, number of guests and time duration of the stays.",
+  //     tags: [
+  //       {
+  //         name: "react",
+  //         color: "blue-text-gradient",
+  //       },
+  //       {
+  //         name: "mongodb",
+  //         color: "green-text-gradient",
+  //       },
+  //       {
+  //         name: "tailwind",
+  //         color: "pink-text-gradient",
+  //       },
+  //     ],
+  //     image: vacationbnb,
+  //     source_code_link: "https://github.com/vicky0h/Vacationbnb.git",
+  //   },
+  //   {
+  //     name: "Expense Tracker",
+  //     description:
+  //       "A web-based platform that allows user to record, track, and compare all incomes and expenditures so that the user can have a better understanding of his/her own financial standing for the given period.",
+  //     tags: [
+  //       {
+  //         name: "react",
+  //         color: "blue-text-gradient",
+  //       },
+  //       {
+  //         name: "mongodb",
+  //         color: "green-text-gradient",
+  //       },
+  //       {
+  //         name: "styled-components css",
+  //         color: "pink-text-gradient",
+  //       },
+  //     ],
+  //     image: expensetracker,
+  //     source_code_link: "https://github.com/vicky0h/ExpenseTracker.git",
+  //   },
+  //   {
+  //     name: "Sudoku",
+  //     description:
+  //       "A web-based sodoku game that enables user to select the difficulty level of the game, check whether the user is on the right track, and generate the solution.",
+  //     tags: [
+  //       {
+  //         name: "react",
+  //         color: "blue-text-gradient",
+  //       },
+  //       {
+  //         name: "materialize css",
+  //         color: "pink-text-gradient",
+  //       },
+  //     ],
+  //     image: sudoku,
+  //     source_code_link: "https://github.com/vicky0h/Sudoku.git",
+  //   },
+  //   {
+  //     name: "Pomodoro Timer",
+  //     description:
+  //       "A web-based productivity application that allows user to set the length of focus and break time. It helps the user to stay focused on a single task until the timer rings, and be more productive.",
+  //     tags: [
+  //       {
+  //         name: "react",
+  //         color: "blue-text-gradient",
+  //       },
+  //       {
+  //         name: "materialize css",
+  //         color: "pink-text-gradient",
+  //       },
+  //     ],
+  //     image: pomodoro,
+  //     source_code_link: "https://github.com/vicky0h/Pomodoro.git",
+  //   },
+  //   {
+  //     name: "Financial Calculator",
+  //     description:
+  //       "A web-based application that enables user to perform regular mathematical calculation as well as financial calculation such as present value, future value, internal rate of return, etc.",
+  //     tags: [
+  //       {
+  //         name: "javascript",
+  //         color: "green-text-gradient",
+  //       },
+  //       {
+  //         name: "css",
+  //         color: "pink-text-gradient",
+  //       },
+  //     ],
+  //     image: financialcalculator,
+  //     source_code_link: "https://github.com/vicky0h/FinancialCalculator.git",
+  //   },
+  // ];
+
   const projects = [
     {
       name: "Dental Clinic",
@@ -163,6 +280,7 @@ import {
       ],
       image: dentalclinic,
       source_code_link: "https://github.com/vicky0h/DentalClinic.git",
+      live_demo_link: "https://dentalclinic-booking.netlify.app/",
     },
     {
       name: "Vacationbnb",
@@ -184,6 +302,7 @@ import {
       ],
       image: vacationbnb,
       source_code_link: "https://github.com/vicky0h/Vacationbnb.git",
+      live_demo_link: "https://vacationbnb.netlify.app/",
     },
     {
       name: "Expense Tracker",
@@ -205,6 +324,7 @@ import {
       ],
       image: expensetracker,
       source_code_link: "https://github.com/vicky0h/ExpenseTracker.git",
+      live_demo_link: "https://expandrev-tracker.netlify.app/",
     },
     {
       name: "Sudoku",
@@ -222,6 +342,7 @@ import {
       ],
       image: sudoku,
       source_code_link: "https://github.com/vicky0h/Sudoku.git",
+      live_demo_link: "https://vicky0h.github.io/Sudoku/",
     },
     {
       name: "Pomodoro Timer",
@@ -239,6 +360,7 @@ import {
       ],
       image: pomodoro,
       source_code_link: "https://github.com/vicky0h/Pomodoro.git",
+      live_demo_link: "https://vicky0h.github.io/Pomodoro/",
     },
     {
       name: "Financial Calculator",
@@ -256,7 +378,8 @@ import {
       ],
       image: financialcalculator,
       source_code_link: "https://github.com/vicky0h/FinancialCalculator.git",
+      live_demo_link: "https://vicky0h.github.io/FinancialCalculator/",
     },
   ];
-  
+
   export { services, technologies, experiences, projects };
