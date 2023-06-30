1535b42256337cb5d4d220721b83ea6e853ecc24
Vicky H <63320567+vicky0h@users.noreply.github.com>
2023-06-30 11:38:58 +0800

    Update Works.jsx weblogo div

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -27,8 +27,6 @@ const ProjectCard = ({
             <div onClick={() => window.open(live_demo_link, "_blank")} className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer">
               <img src={weblogo} alt="live demo" className="w-1/2 h-1/2 object-contain"/>
             </div>
-          </div>
-          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
             <div onClick={() => window.open(source_code_link, "_blank")} className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer">
               <img src={github} alt="github" className="w-1/2 h-1/2 object-contain"/>
             </div>
