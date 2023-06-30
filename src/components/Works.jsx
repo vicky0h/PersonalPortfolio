1f7f88d93f01a7dd7ffe2809d32b7724803df012
Vicky H <63320567+vicky0h@users.noreply.github.com>
2023-06-30 12:02:27 +0800

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -23,7 +23,7 @@ const ProjectCard = ({
       >
         <div className="relative w-full h-[230px]">
           <img src={image} alt={name} className="w-full h-full object-cover rounded-2xl" />
-          <div className="absolute inset-0 flex justify-end m-3 card-img_hover gap-2">
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover gap-1">
             <div onClick={() => window.open(live_demo_link, "_blank")} className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer">
               <img src={weblogo} alt="live demo" className="w-1/2 h-1/2 object-contain"/>
             </div>
